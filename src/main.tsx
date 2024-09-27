77da6f60a7a7d2b1cf95af828641528c16aee3c7
August Dedenroth <augu0577@stud.kea.dk>
2024-09-27 15:16:40 +0200

    Added Darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import theme from "./theme.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-)
+{
+  /*Fetches the body of the index page and asks it to render the things inside App.tsx*/
+}
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
