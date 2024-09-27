1bb27cc8f339e289c49f66e14a8f5d6bd306686c
August Dedenroth <augu0577@stud.kea.dk>
2024-09-27 13:48:42 +0200

    Created basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
 function App() {
-
   return (
-      
-)}
+    <Grid
+    templateAreas={{
+      base: `"nav"
+              "main"`,
+      lg: `"nav"
+           "main"`,
+    }}
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
 
-export default App
+export default App;
