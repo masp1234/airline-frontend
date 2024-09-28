f2e6c8b4b49fc673b9d089f4895475f704e117bb
August Dedenroth <augu0577@stud.kea.dk>
2024-09-28 14:31:04 +0200

    Added profile icon image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,26 @@
 import { HStack, Image, useColorMode } from "@chakra-ui/react";
 import logoDark from "../assets/logo-dark.webp";
 import logoLight from "../assets/logo-light.webp";
+import profileIconDark from "../assets/profile-dark.webp";
+import profileIconLight from "../assets/profile-bright.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import NavButtons from "./NavButtons";
 
 const NavBar = () => {
   const { colorMode } = useColorMode();
   const logo = colorMode === "dark" ? logoDark : logoLight;
+  const profileIcon = colorMode === "dark" ? profileIconDark : profileIconLight;
 
   return (
     <>
       <HStack justifyContent="space-between" padding="10px">
         {/* HStack is a horizontal stack, Use Vstack for vertical. */}
-        {/* Normally you shouldn't give width, ml, etc. but it squished the image otherwise. */}
+        {/* Normally you shouldn't give width, ml, etc. but it squished the logo-image otherwise. */}
         <Image src={logo} width="100px" ml="20px" mr="20px" />
         <NavButtons />
         <ColorModeSwitch />
+        {/* TODO: Replace image with button? */}
+        <Image src={profileIcon} width="30px" ml="20px" mr="20px" />
       </HStack>
     </>
   );
