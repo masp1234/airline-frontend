0164e55b8fe3ce7e1579e9be48b1d6bf702dace9
August Dedenroth <augu0577@stud.kea.dk>
2024-09-28 14:20:38 +0200

    Added Navbar buttons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { HStack, Image, useColorMode } from "@chakra-ui/react";
 import logoDark from "../assets/logo-dark.webp";
 import logoLight from "../assets/logo-light.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import NavButtons from "./NavButtons";
 
 const NavBar = () => {
   const { colorMode } = useColorMode();
@@ -12,7 +13,8 @@ const NavBar = () => {
       <HStack justifyContent="space-between" padding="10px">
         {/* HStack is a horizontal stack, Use Vstack for vertical. */}
         {/* Normally you shouldn't give width, ml, etc. but it squished the image otherwise. */}
-        <Image src={logo} width="100px" ml="20px" mr="20px" />{" "}
+        <Image src={logo} width="100px" ml="20px" mr="20px" />
+        <NavButtons />
         <ColorModeSwitch />
       </HStack>
     </>
---
+++ src/components/NavButtons.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonGroup, HStack } from "@chakra-ui/react";
+
+const NavButtons = () => {
+  return (
+    <HStack>
+      <Button variant="link">
+        Flights
+      </Button>
+      <Button variant="link">
+        My Bookings
+      </Button>
+    </HStack>
+  );
+};
+
+export default NavButtons;
