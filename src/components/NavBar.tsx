1c1b9bb6119e277abbd9d4de3e0ef5668a80cf60
August Dedenroth <augu0577@stud.kea.dk>
2024-09-28 13:08:48 +0200

    Added toggle-darkmode switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <>
-      <HStack>
+      <HStack justifyContent="space-between" padding="10px">
         {/* HStack is a horizontal stack, Use Vstack for vertical. */}
         {/* Normally you shouldn't give width, ml, etc. but it squished the image otherwise. */}
         <Image src={logo} width="100px" ml="20px" mr="20px" />{" "}
-        <Text>NavBar</Text>
+        <ColorModeSwitch />
       </HStack>
     </>
   );
