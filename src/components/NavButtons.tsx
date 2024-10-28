3b4320e75766e670b26e241935c50e7c5ed34389
RenasAli <rena0506@stud.kea.dk>
2024-10-28 13:30:27 +0100

    add a hamburger menu

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -1,14 +1,11 @@
-import {  HStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
 const NavButtons = () => {
   return (
-    <HStack spacing={50} justifyContent="flex-start" width="70%">
+    <>
       <Link to="/" >Flights</Link>
       <Link to="/my-bookings" >My Bookings</Link>
-      {/*<Button variant="link" size={"lg"}  >Flights</Button>
-      <Button variant="link" size={"lg"}>My Bookings</Button>*/}
-    </HStack>
+    </>
+    
   );
 };
 
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,29 @@
-import { HStack, Image, useColorMode } from "@chakra-ui/react";
+import { HStack, Image, useColorMode, 
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  useDisclosure,
+  Show,
+  Hide,
+  VStack,
+} from "@chakra-ui/react";
 import logoDark from "../assets/logo-dark.webp";
 import logoLight from "../assets/logo-light.webp";
 import profileIconDark from "../assets/profile-dark.webp";
 import profileIconLight from "../assets/profile-bright.webp";
 import ColorModeSwitch from "../components/ColorModeSwitch";
 import NavButtons from "../components/NavButtons";
+import { FiMenu } from "react-icons/fi";
+
+//import { IoCloseSharp } from "react-icons/io5";
 
 const Header = () => {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
+  const { isOpen, onOpen, onClose} = useDisclosure()
+
   const logo = colorMode === "dark" ? logoDark : logoLight;
   const profileIcon = colorMode === "dark" ? profileIconDark : profileIconLight;
   return (
@@ -16,10 +32,37 @@ const Header = () => {
       {/* HStack is a horizontal stack, Use VStack for vertical. */}
       {/* Normally you shouldn't give width, ml, etc. but it squished the logo-image otherwise. */}
       <Image src={logo} width="100px" ml="20px" mr="20px" />
-      <NavButtons />
-      <ColorModeSwitch />
+      <Show above="lg">
+        <HStack spacing={50} justifyContent="flex-start" width="70%">
+          <NavButtons/>
+        </HStack>
+        
+        <ColorModeSwitch />
+        
+      </Show>
       {/* TODO: Replace image with button? */}
       <Image src={profileIcon} width="40px" ml="20px" mr="20px" />
+      <Hide above="lg">
+        <FiMenu onClick={onOpen} size={35}/>
+      </Hide>
+
+      *<Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}      >
+        <DrawerOverlay />
+        <DrawerContent
+        onClick={onClose}>
+          <DrawerCloseButton />
+          <DrawerHeader></DrawerHeader>
+          <DrawerBody>
+            <VStack spacing={25} width="70%">
+              <NavButtons />
+              <ColorModeSwitch />
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </HStack>
   </>
   )
