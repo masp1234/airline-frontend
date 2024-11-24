21ba56c80ad765c0dc7d6bba2b74f1d6319065c1
August Dedenroth <augu0577@stud.kea.dk>
2024-11-24 12:35:48 +0100

    Fixed bug on ProfileIcon

--- src/components/ProfileIcon.tsx
+++ src/components/ProfileIcon.tsx
@@ -10,7 +10,7 @@ import {
 } from "@chakra-ui/react";
 import profileIconDark from "../assets/profile-dark.webp";
 import profileIconLight from "../assets/profile-bright.webp";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { NoRole, RoleGuard } from "../auth/RoleGuard";
 import useRoleStore from "../store";
 import BASE_URL from "../util/baseUrl";
@@ -23,7 +23,7 @@ const ProfileIcon = () => {
   const dropDownHoverColor = colorMode === "dark" ? "#181D29" : "#EDF2F7";
 
   const toast = useToast();
-  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     try {
@@ -31,10 +31,9 @@ const ProfileIcon = () => {
         method: "POST",
         credentials: "include",
       });
-  
+
       if (response.ok) {
         useRoleStore.getState().setRole(null);
-        setRedirect(true);
         toast({
           title: "Logged out",
           description: "You have successfully logged out",
@@ -42,6 +41,7 @@ const ProfileIcon = () => {
           duration: 4000,
           isClosable: true,
         });
+        navigate("/");
       } else {
         toast({
           title: "Logout failed",
@@ -49,16 +49,19 @@ const ProfileIcon = () => {
           status: "error",
           duration: 4000,
           isClosable: true,
-        })
+        });
       }
     } catch (error) {
       console.error("Error during logout:", error);
+      toast({
+        title: "Logout failed",
+        description: "Something went wrong",
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
     }
   };
-
-  if (redirect) {
-    return <Navigate to="/" />;
-  }
 
   return (
     <Menu>
@@ -66,7 +69,6 @@ const ProfileIcon = () => {
         as={Box}
         cursor="pointer"
         _hover={{ backgroundColor: "blackAlpha.200" }} // TODO: Figure out how to make the highlight cover the whole vertical grid-space. Or maybe just delete it.
-       // height="100%"
       >
         <Image
           src={profileIcon}
@@ -79,54 +81,51 @@ const ProfileIcon = () => {
         />
       </MenuButton>
       <MenuList backgroundColor={dropDownColor}>
-      <NoRole>
-        <Link to="/signup">
+        <NoRole>
+          <Link to="/signup">
+            <MenuItem
+              backgroundColor={dropDownColor}
+              _hover={{ backgroundColor: dropDownHoverColor }}
+            >
+              <Box>
+                <p>User Signup</p>
+              </Box>
+            </MenuItem>
+          </Link>
+        </NoRole>
+        <NoRole>
+          <Link to="/login">
+            <MenuItem
+              backgroundColor={dropDownColor}
+              _hover={{ backgroundColor: dropDownHoverColor }}
+            >
+              <Box>
+                <p>User Login</p>
+              </Box>
+            </MenuItem>
+          </Link>
+        </NoRole>
+        <RoleGuard allowedRoles={["Admin", "Customer"]}>
           <MenuItem
             backgroundColor={dropDownColor}
             _hover={{ backgroundColor: dropDownHoverColor }}
           >
             <Box>
-              <p>User Signup</p>
+              <p>My Profile</p> {/* Purely to add a bit more content to the dropdown */}
             </Box>
           </MenuItem>
-        </Link>
-      </NoRole>
-      <NoRole>
-        <Link to="/login">
+        </RoleGuard>
+        <RoleGuard allowedRoles={["Admin", "Customer"]}>
           <MenuItem
             backgroundColor={dropDownColor}
             _hover={{ backgroundColor: dropDownHoverColor }}
+            onClick={handleLogout}
           >
             <Box>
-              <p>User Login</p>
+              <p>User logout</p>
             </Box>
           </MenuItem>
-        </Link>
-      </NoRole>
-      <RoleGuard allowedRoles={["Admin", "Customer"]}>
-        <MenuItem
-          backgroundColor={dropDownColor}
-          _hover={{ backgroundColor: dropDownHoverColor }}
-        >
-          <Box>
-            <p>My Profile</p> {/* Purely to add a bit more content to the dropdown */}
-          </Box>
-        </MenuItem>
-      </RoleGuard>
-      <RoleGuard allowedRoles={["Admin", "Customer"]}>
-        <Link to="/" onClick={
-          handleLogout
-          }>
-        <MenuItem
-          backgroundColor={dropDownColor}
-          _hover={{ backgroundColor: dropDownHoverColor }}
-        >
-          <Box>
-            <p>User logout</p>
-          </Box>
-        </MenuItem>
-        </Link>
-      </RoleGuard>
+        </RoleGuard>
       </MenuList>
     </Menu>
   );
