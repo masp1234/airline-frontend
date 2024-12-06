e1f8bba32c280d87f9b0733e5ccd751b92cc9a4e
August Høg Dedenroth <augu0577@stud.kea.dk>
2024-12-06 18:42:40 +0100

    Return roleguard to App (#22)

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,116 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Container from "./sections/Container.tsx";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Header from "./sections/Header";
-import Home from './pages/Home';
-import MyBookings from './pages/MyBookings';
-import SignUp from './pages/SignUp';
-import Login from './pages/Login';
-import CreateFlight from './pages/CreateFlight';
+import Home from "./pages/Home";
+import MyBookings from "./pages/MyBookings";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import CreateFlight from "./pages/CreateFlight";
 import FindTicket from "./pages/FindTicket.tsx";
 import BookingTickets from "./pages/BookingTickets.tsx";
 import ManageFlight from "./pages/ManageFlight.tsx";
 import ManageFlights from "./pages/ManageFlights.tsx";
+import { NoRoleProtectedRoute, RoleProtectedRoute } from "./auth/RoleGuard.tsx";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <Grid
-            templateAreas={{
-              base: `"nav"
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Grid
+          templateAreas={{
+            base: `"nav"
                 "main"`,
-              lg: `"nav"
+            lg: `"nav"
             "main"`,
-      }}
-      >
-        <GridItem pl="2" area={"nav"}>
-          <Header />
-        </GridItem>
-        <GridItem pl="2" padding={"4x"} position={"relative"} area={"main"}>
-          <Container>
-            <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/my-bookings' element={<MyBookings/>}/>
-            <Route path='/signup' element={<SignUp/>}/>
-            <Route path='/create-flight' element={<CreateFlight/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/find-ticket/departure' element={<FindTicket/>}/>
-            <Route path='/find-ticket/return' element={<FindTicket/>}/>
-            <Route path='/booking' element={<BookingTickets/>}/>
-            <Route path='/manage-flights' element={<ManageFlights/>}/>
-            <Route path='/manage-flights/:flightId' element={<ManageFlight/>}/>
-            </Routes>
-          </Container>
-        </GridItem>
-      </Grid>
+          }}
+        >
+          <GridItem pl="2" area={"nav"}>
+            <Header />
+          </GridItem>
+          <GridItem pl="2" padding={"4x"} position={"relative"} area={"main"}>
+            <Container>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                  path="/my-bookings"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Customer", "Admin"]}>
+                      <MyBookings />
+                    </RoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/signup"
+                  element={
+                    <NoRoleProtectedRoute>
+                      <SignUp />
+                    </NoRoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/create-flight"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Admin"]}>
+                      <CreateFlight />
+                    </RoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/login"
+                  element={
+                    <NoRoleProtectedRoute>
+                      <Login />
+                    </NoRoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/find-ticket/departure"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Customer", "Admin"]}>
+                      <FindTicket />
+                    </RoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/find-ticket/return"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Customer", "Admin"]}>
+                      <FindTicket />
+                    </RoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/booking"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Customer", "Admin"]}>
+                      <BookingTickets />
+                    </RoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/manage-flights"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Admin"]}>
+                      <ManageFlights />
+                    </RoleProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/manage-flights/:flightId"
+                  element={
+                    <RoleProtectedRoute allowedRoles={["Admin"]}>
+                      <ManageFlight />
+                    </RoleProtectedRoute>
+                  }
+                />
+              </Routes>
+            </Container>
+          </GridItem>
+        </Grid>
       </Router>
     </QueryClientProvider>
-
   );
 }
 
