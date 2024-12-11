000ba6d1d3feb12de17e1a63f07084b4c526c25a
masp1234 <mart2c04@stud.kea.dk>
2024-12-11 13:40:07 +0100

    try changing the day offset

--- cypress/e2e/02_create-booking.cy.js
+++ cypress/e2e/02_create-booking.cy.js
@@ -34,7 +34,7 @@ describe('Booking - a customer should be able to search for flights and create a
       const currentDate = new Date();
 
       cy.get(".react-datepicker__day")
-        .contains(currentDate.getDate() + 4)
+        .contains(currentDate.getDate() + 3)
         .click();
         
       cy.contains("Return Date")
@@ -42,7 +42,7 @@ describe('Booking - a customer should be able to search for flights and create a
         .find("[type='text']")
         .click();
 
-        cy.get(".react-datepicker__day").contains(currentDate.getDate() + 9).click();
+        cy.get(".react-datepicker__day").contains(currentDate.getDate() + 8).click();
 
         cy.get("button").contains("Search for Flights").click();
 
