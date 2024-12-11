c7e64eb712d3b48c1f67c91065f9f9e25426d976
August Høg Dedenroth <augu0577@stud.kea.dk>
2024-12-11 17:03:06 +0100

    My bookings info (#31)
    * Bookings query
    
    * Flightcard per booking
    
    * Neat little hover-effect on card
    
    * Display ticket info (broke)
    
    * Ticket info works now
    
    * stuff
    
    * Proper ticket display and other fixes
    
    * Fix typing
    
    * Add departure/arrival time and fix interface
    
    * Formatting fix
    
    * More formatting

---
+++ src/pages/TicketInfo.tsx
@@ -0,0 +1,46 @@
+import { Divider, GridItem, Heading, SimpleGrid } from "@chakra-ui/react";
+import BookingTicketInfo from "../components/BookingTicketInfo";
+import { useParams } from "react-router-dom";
+import useBookings from "../hooks/useBookings";
+import { useAppSelector } from "../hooks/useRedux";
+import { Ticket } from "../types/Ticket";
+import { Booking } from "../types/Booking";
+
+const TicketInfo = () => {
+  const { bookingId } = useParams<{ bookingId: string }>();
+  const email = useAppSelector((state) => state.loginUserData.data?.email);
+  const { bookingsQuery } = useBookings(email);
+
+  if (bookingsQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (bookingsQuery.isError) {
+    return <div>Error loading booking information.</div>;
+  }
+
+  // Finds the specific booking by ID
+  const booking = bookingsQuery.data?.find(
+    (b: Booking) => b.id?.toString() === bookingId
+  );
+
+  if (!booking) {
+    return <div>No booking found for the given ID.</div>;
+  }
+
+  return (
+    <GridItem gridArea="main" bg="blackAlpha.200" padding="4" boxShadow="lg">
+      <Heading size="xl" mb="4">
+        Ticket Information for booking: {booking.confirmationNumber}
+      </Heading>
+      <Divider orientation="horizontal" mb="4" w="100%" />
+      <SimpleGrid minChildWidth="49%" spacing={5}>
+        {booking.tickets?.map((ticket: Ticket) => (
+          <BookingTicketInfo key={ticket.ticketNumber} ticketInfo={ticket} />
+        ))}
+      </SimpleGrid>
+    </GridItem>
+  );
+};
+
+export default TicketInfo;
--- src/types/Ticket.ts
+++ src/types/Ticket.ts
@@ -4,4 +4,16 @@ export interface Ticket{
     passenger: Passenger | null,
     flightId: number | null,
     flightClassId: number | null,
+    ticketNumber: string | null,
+    price: number | null,
+    flightCode: string | null,
+    departurePortName: string | null,
+    arrivalPortName: string | null,
+    flightClassName: string | null,
+    passengerFirstName: string | null,
+    passengerLastName: string | null,
+    passengerEmail: string | null,
+    flightTravelTime: number | null,
+    flightDepartureTime: Date | null,
+    flightCompletionTime: Date | null
 }
